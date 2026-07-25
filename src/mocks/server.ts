82cd77a4f425bc9e7b841a96dfb261c7c731674e
rondido <rppr001@naver.com>
2026-07-25 22:21:16 +0900

    test: 컬렉션 목록 조회(전체,획득,미획득) 테스트 구현

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,12 +2,14 @@ import { setupServer } from 'msw/node';
 
 import { handlers as authHandlers } from './api/auth';
 import { handlers as categoryHandlers } from './api/category';
+import { handlers as collectionHandlers } from './api/collection';
 import { handlers as missionHandlers } from './api/mission';
 import { handlers as userHandlers } from './api/user';
 
 export const server = setupServer(
   ...authHandlers,
   ...categoryHandlers,
+  ...collectionHandlers,
   ...missionHandlers,
   ...userHandlers,
 );
